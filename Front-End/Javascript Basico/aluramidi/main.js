df7690611cffdefaabaabc4a683fd70680641b8d
Waldir Marques <waldirwolf@outlook.com>
2023-11-27 02:36:14 -0300

    novo projeto

--- Front-End/Javascript Basico/aluramidi/main.js
+++ Front-End/Javascript Basico/aluramidi/main.js
@@ -1,19 +1,38 @@
-
-function tocasom(idelentoaudio) {
-    document.querySelector(idelentoaudio).play();
+//fazendo tocar 
+function tocarsom(idelementoaudio) {
+        document.querySelector(idelementoaudio).play()
 }
 
-const listadeteclas = document.querySelectorAll('.tecla');
+//Pela todas as classes e cria uma arrey 
+ const listateclas = document.querySelectorAll('.tecla')
+
+ //Atribuindo a função onclick para todos on indices da lista
+
+ for (let contador = 0 ; contador < listateclas.length; contador ++){
+    const tecla = listateclas[contador]
+
+    const instrumento = tecla.classList[1]
+
+    const idaudio = `#som_${instrumento}`
+   
+    tecla.onclick = function(){
+        tocarsom(idaudio)
+    } 
 
 
-let contador = 0;
-//enquanto
-while (contador < listadeteclas.length) {
+    //Adcinionando uma classe no buttom quando precinar enter
+    tecla.onkeydown = function(evento){
+       // console.log(evento)
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa')
 
-    listadeteclas[contador].onclick =  function () {
-        tocasom('')
-    };
+        }
+        
+    }
 
-    contador = contador + 1;
+    //Remove a classe no buttom quando precinar enter
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
 }
 
